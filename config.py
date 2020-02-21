d050e096b1dd4ad2ffe4165f4088722665abdff4
Espoir Murhabazi <espoir.mur@gmail.com>
2020-02-21 12:03:56 +0200

    - Task Completed : add redis store to retrieve the last time I tweeted
    - Next Task :check on th todos

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import logging
+from redis import Redis
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
+redis_store = Redis(host='localhost', port=6379, db=0)
--- run.py
+++ run.py
@@ -1,15 +1,21 @@
 from scripts.bot import TwitterBot
 from scripts.twitter_client import get_twitter_client
-from config import logger
+from config import logger, redis_store
 from time import sleep
 from scripts.utils import load_models
 
+
 if __name__ == "__main__":
     api = get_twitter_client()
-    since_id = 1
+    since_id = redis_store.get('since_id')
+    if not since_id:
+        since_id = 1
+    else:
+        since_id = int(since_id)
     the_models = load_models()
     while True:
         the_bot = TwitterBot(api, since_id, the_models)
         since_id = the_bot.check_mentions()
+        redis_store.set('since_id', since_id)
         logger.info(f"Waiting...", )
         sleep(60)
